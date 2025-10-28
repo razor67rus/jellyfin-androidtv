de3e6fa6a1b3e63c72c9991a49997fc2507fbd14
razor67rus <razor67rus@gmail.com>
2025-10-28 14:55:02 +0300

    Статус бар и работающее избранное

--- app/src/main/java/org/jellyfin/androidtv/ui/browsing/grid/BrowseGridViewModel.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/browsing/grid/BrowseGridViewModel.kt
@@ -1,6 +1,5 @@
 package org.jellyfin.androidtv.ui.browsing.grid
 
-import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import androidx.lifecycle.AndroidViewModel
@@ -12,6 +11,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.constant.GridDirection
 import org.jellyfin.androidtv.constant.ImageType
 import org.jellyfin.androidtv.constant.PosterSize
@@ -22,7 +22,16 @@ import org.jellyfin.androidtv.ui.itemhandling.BaseRowItem
 import org.jellyfin.androidtv.ui.itemhandling.ItemRowAdapterWrapper
 import org.jellyfin.androidtv.ui.presentation.CardPresenter
 import org.jellyfin.sdk.model.api.BaseItemDto
+import org.jellyfin.sdk.model.api.CollectionType
+import org.jellyfin.sdk.model.api.ItemSortBy
+import org.jellyfin.sdk.model.api.SortOrder
 import timber.log.Timber
+
+data class SortOption(
+	val name: String,
+	val value: ItemSortBy,
+	val order: SortOrder
+)
 
 class BrowseGridViewModel(
 	application: Application,
@@ -33,9 +42,6 @@ class BrowseGridViewModel(
 	private val context: Context get() = getApplication<Application>().applicationContext
 
 	private var adapterWrapper: ItemRowAdapterWrapper? = null
-
-    private val _itemsTest = MutableStateFlow<List<Int>>(emptyList())
-    val itemsTest = _itemsTest.asStateFlow()
 
 	private val _posterSize = MutableStateFlow(libraryPreferences[LibraryPreferences.posterSize])
 	val posterSize: StateFlow<PosterSize> = _posterSize.asStateFlow()
@@ -52,6 +58,9 @@ class BrowseGridViewModel(
 	private  val _filterUnwatchedOnly = MutableStateFlow(libraryPreferences[LibraryPreferences.filterUnwatchedOnly])
 	val filterUnwatchedOnly: StateFlow<Boolean> = _filterUnwatchedOnly.asStateFlow()
 
+	private val _sortBy = MutableStateFlow(libraryPreferences[LibraryPreferences.sortBy])
+	val sortBy: StateFlow<ItemSortBy?> = _sortBy.asStateFlow()
+
 	private val _items = MutableStateFlow<List<BaseRowItem>>(emptyList())
 	val items: StateFlow<List<BaseRowItem>> = _items.asStateFlow()
 
@@ -61,9 +70,25 @@ class BrowseGridViewModel(
 	private val _totalItems = MutableStateFlow(0)
 	val totalItems: StateFlow<Int> = _totalItems.asStateFlow()
 
-    init {
-		loadItemsTest()
-    }
+    private val _selectedIndex = MutableStateFlow(0)
+    val selectedIndex = _selectedIndex.asStateFlow()
+
+	private val _filters = MutableStateFlow<FilterOptions?>(null)
+	val filters: StateFlow<FilterOptions?> = _filters.asStateFlow()
+
+	private val _startLetter = MutableStateFlow<String?>(null)
+	val startLetter: StateFlow<String?> = _startLetter.asStateFlow()
+
+	private val _sortOptions = MutableStateFlow<Map<Int, SortOption>>(emptyMap())
+	val sortOptions: StateFlow<Map<Int, SortOption>> = _sortOptions.asStateFlow()
+
+
+
+	init {
+		// 2. Инициализируем опции сортировки в блоке init
+		initializeSortOptions()
+	}
+
 
 	fun initializeAdapter(
 		cardPresenter: CardPresenter,
@@ -71,6 +96,7 @@ class BrowseGridViewModel(
 		chunkSize: Int,
 		lifecycle: Lifecycle
 	) {
+		cardPresenter.setUniformAspect(true); // make grid layouts always uniform
 		val adapter = ItemRowAdapterWrapper.createAdapter(context, libraryPreferences, cardPresenter, rowDef, chunkSize)
 		adapterWrapper = ItemRowAdapterWrapper(adapter, viewModelScope, lifecycle)
 
@@ -90,31 +116,46 @@ class BrowseGridViewModel(
 				_totalItems.value = it
 			}
 		}
+		viewModelScope.launch {
+			adapterWrapper?.filters?.collect {
+				_filters.value = it
+			}
+		}
+		viewModelScope.launch {
+			adapterWrapper?.startLetter?.collect {
+				_startLetter.value = it
+			}
+		}
+		viewModelScope.launch {
+			adapterWrapper?.sortBy?.collect {
+				if (it != null) {
+					_sortBy.value = it
+				}
+			}
+		}
 
-		// Загружаем начальные данные
 		adapterWrapper?.retrieve()
 	}
-
-    fun loadItemsTest() {
-        viewModelScope.launch {
-			_itemsTest.value = List(20) { it }
-        }
-    }
 
     fun refreshPreferences() {
         _posterSize.value = libraryPreferences[LibraryPreferences.posterSize]
         _imageType.value = libraryPreferences[LibraryPreferences.imageType]
 		_gridDirection.value = libraryPreferences[LibraryPreferences.gridDirection]
-		loadItemsTest() // Перезагружаем элементы с новыми настройками
     }
 
 	fun toggleFavoriteFilter() {
 		viewModelScope.launch {
 			val newValue = !filterFavoritesOnly.value
 			libraryPreferences[LibraryPreferences.filterFavoritesOnly] = newValue
-			_filterFavoritesOnly.value = newValue
 			libraryPreferences.commit()
+			adapterWrapper?.setFilters(
+				FilterOptions().apply {
+					isFavoriteOnly = newValue
+					isUnwatchedOnly = filterUnwatchedOnly.value
+				}
+			)
 			adapterWrapper?.retrieve()
+			_filterFavoritesOnly.value = newValue
 		}
 	}
 
@@ -122,10 +163,51 @@ class BrowseGridViewModel(
 		viewModelScope.launch {
 			val newValue = !filterUnwatchedOnly.value
 			libraryPreferences[LibraryPreferences.filterUnwatchedOnly] = newValue
-			_filterUnwatchedOnly.value = newValue
 			libraryPreferences.commit()
+			adapterWrapper?.setFilters(
+				FilterOptions().apply {
+					isFavoriteOnly = filterFavoritesOnly.value
+					isUnwatchedOnly = newValue
+				}
+			)
 			adapterWrapper?.retrieve()
+			_filterUnwatchedOnly.value = newValue
 		}
+	}
+
+	fun setSelectedIndex(index: Int) {
+        _selectedIndex.value = index
+    }
+
+	fun setSortBy(option: SortOption) {
+//		viewModelScope.launch {
+//			adapterWrapper?.setSortBy(option.value, option.order)
+//			adapterWrapper?.retrieve()
+//		}
+	}
+
+	private fun initializeSortOptions() {
+
+		val sortOptions = mutableMapOf(
+			0 to SortOption(context.getString(R.string.lbl_name), ItemSortBy.SORT_NAME, SortOrder.ASCENDING),
+			1 to SortOption(context.getString(R.string.lbl_date_added), ItemSortBy.DATE_CREATED, SortOrder.DESCENDING),
+			2 to SortOption(context.getString(R.string.lbl_premier_date), ItemSortBy.PREMIERE_DATE, SortOrder.DESCENDING),
+			3 to SortOption(context.getString(R.string.lbl_rating), ItemSortBy.OFFICIAL_RATING, SortOrder.ASCENDING),
+			4 to SortOption(context.getString(R.string.lbl_community_rating), ItemSortBy.COMMUNITY_RATING, SortOrder.DESCENDING),
+			5 to SortOption(context.getString(R.string.lbl_critic_rating), ItemSortBy.CRITIC_RATING, SortOrder.DESCENDING)
+		)
+
+		if (folder.collectionType == CollectionType.TVSHOWS) {
+			sortOptions[6] = SortOption(context.getString(R.string.lbl_last_played), ItemSortBy.SERIES_DATE_PLAYED, SortOrder.DESCENDING)
+		} else {
+			sortOptions[6] = SortOption(context.getString(R.string.lbl_last_played), ItemSortBy.DATE_PLAYED, SortOrder.DESCENDING)
+		}
+
+		if (folder.collectionType == CollectionType.MOVIES) {
+			sortOptions[7] = SortOption(context.getString(R.string.lbl_runtime), ItemSortBy.RUNTIME, SortOrder.ASCENDING)
+		}
+
+		_sortOptions.value = sortOptions
 	}
 }
 
